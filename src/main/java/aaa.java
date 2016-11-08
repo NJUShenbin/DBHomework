921bd79a0a6a76265df3db453cd681580790434b
申彬 <705103197@qq.com>
2016-11-08 10:37:50 +0800

    commit

--- src/main/java/aaa.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by sbin on 2016/11/8.
- */
-public class aaa {
-    public aaa(){
-        new Thread(()-> System.out.println("aa"));
-    }
-}
---
+++ src/main/java/edu/nju/dbhomework/DbHomeworkApplication.java
@@ -0,0 +1,12 @@
+package edu.nju.dbhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbHomeworkApplication.class, args);
+	}
+}
